dc6724d6188ccfff7c4b7609ba065927c92a51cd
AI-DLC Agent <ai-dlc@example.com>
2026-03-16 14:20:08 +0000

    Refactor Item component to use Cloudscape Checkbox and Button

--- examples/react/src/todo/components/item.jsx
+++ examples/react/src/todo/components/item.jsx
@@ -1,5 +1,7 @@
 import { memo, useState, useCallback } from "react";
 import classnames from "classnames";
+import Checkbox from "@cloudscape-design/components/checkbox";
+import Button from "@cloudscape-design/components/button";
 
 import { Input } from "./input";
 
@@ -40,11 +42,11 @@ export const Item = memo(function Item({ todo, dispatch, index }) {
                     <Input onSubmit={handleUpdate} label="Edit Todo Input" defaultValue={title} onBlur={handleBlur} />
                 ) : (
                     <>
-                        <input className="toggle" type="checkbox" data-testid="todo-item-toggle" checked={completed} onChange={toggleItem} />
+                        <Checkbox checked={completed} onChange={toggleItem} data-testid="todo-item-toggle" />
                         <label data-testid="todo-item-label" onDoubleClick={handleDoubleClick}>
                             {title}
                         </label>
-                        <button className="destroy" data-testid="todo-item-button" onClick={removeItem} />
+                        <Button variant="icon" iconName="close" data-testid="todo-item-button" onClick={removeItem} />
                     </>
                 )}
             </div>
